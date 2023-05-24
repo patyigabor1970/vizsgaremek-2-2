e797d8a7ecb59bd0cfa0d1c5cff7121647f461ee
patyigabor1970 <128298509+patyigabor1970@users.noreply.github.com>
2023-05-24 08:26:43 +0200

    Add files via upload
    újra gomb a register.jsx ben
    a main.jsx ben csak átirogattam a megjelenő felületen azokat a részeket a szövegben, amelyek nem egyeztek a navigációs sáv linkjeivel pl. csapatunk, szolgáltatásaink, áraink...

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,78 +1,153 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './Main.css';
-
-
+import "./Main.css";
+import { useState, useEffect } from "react";
 
 function Main() {
-
-  const navigate=useNavigate();
- 
-const handleButtonClick = (e) => {
+  const navigate = useNavigate();
+  const [access,setAccess] = useState(-1);
+  const [refresh, setRefresh] = useState(false);
+  const token = sessionStorage.getItem("usertoken");
+  const update = () => {
+    setRefresh((prev) => !prev);
+  };
+  const reloadCount = sessionStorage.getItem("reloadCount");
+  
+  
+  
+  
+  if (token) {
+  useEffect(() => {
+    
+      fetch(`http://localhost:80/api/fitness/login2`, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((result) => result.json())
+        .then((adatok) => {
+          if (adatok == "Invalid Token") {
+            alert(adatok);
+          } else {
+            setAccess(adatok);
+            sessionStorage.setItem('access',adatok);
+          }
+          if(reloadCount < 2 && access) {
+            sessionStorage.setItem('reloadCount', String(reloadCount + 1));
+            window.location.reload();
+          } else {
+            sessionStorage.removeItem('reloadCount');
+          }
+        })
+        .catch((err) => console.error(err));
+    
+  }, []);
+}
+  const handleButtonClick = (e) => {
     navigate(e.target.value);
-    }
+  };
+
   return (
     <div>
       <div className="hero max-h-screen bg-red-600">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="src/images/feature-4.jpg" className="max-w-lg rounded-lg shadow-2xl border-8 border-black"/>
+          <img
+            src="src/images/feature-4.jpg"
+            className="max-w-lg rounded-lg shadow-2xl border-8 border-black"
+          />
           <div>
             <h1 className="text-5xl font-bold text-white">Fitness</h1>
             <p className="py-6 text-5xl font-bold text-white text-justify">
-            Edzőtermünk gépparkjának összeállításánál fontos szempont volt, hogy akár a teljesen kezdő, akár a profi bodybuilderek is megtalálják 
-            a számukra megfelelő gépeket. Teremedzőink ingyenes edzéstervet készítenek számodra és segítenek megtanulni a gyakorlatok helyes kivitelezését.
-Edzőtermünkben Hoist, Life Fitness, Hammer Strenght, Panatta gépek széles választékával találkozhattok.
+              Edzőtermünk gépparkjának összeállításánál fontos szempont volt,
+              hogy akár a teljesen kezdő, akár a profi bodybuilderek is
+              megtalálják a számukra megfelelő gépeket. Teremedzőink ingyenes
+              edzéstervet készítenek számodra és segítenek megtanulni a
+              gyakorlatok helyes kivitelezését. Edzőtermünkben Hoist, Life
+              Fitness, Hammer Strenght, Panatta gépek széles választékával
+              találkozhattok.
             </p>
-           
           </div>
         </div>
       </div>
       <div className="hero max-h-screen bg-black">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img id="responsive" src="src/images/edzoink.jpg"className="max-w-lg rounded-lg shadow-2xl border-8 border-red-600"/>
+          <img
+            id="responsive"
+            src="src/images/edzoink.jpg"
+            className="max-w-lg rounded-lg shadow-2xl border-8 border-red-600"
+          />
           <div>
-            <h1 className="text-5xl font-bold text-white">Edzőink</h1>
+            <h1 className="text-5xl font-bold text-white">Csapatunk</h1>
             <p className="py-6 text-5xl font-bold text-white text-justify">
-            Ha szeretnéd hatékonyan és biztonságosan elérni céljaidat válassz magasan képzett személyi edzőink közül. 
-            Edzőink a edzés időtartama alatt csak rád figyelnek, hogy élmény legyen a közös edzés. Vedd fel a kapcsolatot az általad választott edzővel.
+              Ha szeretnéd hatékonyan és biztonságosan elérni céljaidat válassz
+              magasan képzett személyi edzőink közül. Edzőink a edzés időtartama
+              alatt csak rád figyelnek, hogy élmény legyen a közös edzés. Vedd
+              fel a kapcsolatot az általad választott edzővel.
             </p>
-            
-            <button className="btn btn-primary hover:bg-red-600" id="trainers" value='/trainers'onClick={(e)=> handleButtonClick(e)}>Tovább az edzőkhöz</button>
+
+            <button
+              className="btn btn-primary hover:bg-red-600"
+              id="trainers"
+              value="/trainers"
+              onClick={(e) => handleButtonClick(e)}
+            >
+              Tovább az edzőkhöz
+            </button>
           </div>
         </div>
       </div>
       <div className="hero max-h-screen bg-red-600">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="src/images/istockphoto-szolgaltatas.jpg" className="max-w-lg rounded-lg shadow-2xl border-8 border-black"/>
+          <img
+            src="src/images/istockphoto-szolgaltatas.jpg"
+            className="max-w-lg rounded-lg shadow-2xl border-8 border-black"
+          />
           <div>
-            <h1 className="text-5xl font-bold text-white">Szolgáltatások</h1>
+            <h1 className="text-5xl font-bold text-white">Szolgáltatásaink</h1>
             <p className="py-6 text-5xl font-bold text-white text-justify">
-            Ha szereted a CrossFit, spinning, aerobic edzések változatosságát, intenzitását akkor nálunk megvan a lehetőséged, hogy kihozd magadból a maximumot. 
-            
+              Ha szereted a CrossFit, spinning, aerobic edzések változatosságát,
+              intenzitását akkor nálunk megvan a lehetőséged, hogy kihozd
+              magadból a maximumot.
             </p>
-            <button className="btn btn-primary hover:bg-black" id="trainig" value='/training'onClick={(e)=> handleButtonClick(e)}>Tovább a szolgáltatásokhoz</button>
-
+            <button
+              className="btn btn-primary hover:bg-black"
+              id="trainig"
+              value="/training"
+              onClick={(e) => handleButtonClick(e)}
+            >
+              Tovább a szolgáltatásokhoz
+            </button>
           </div>
-      </div>
+        </div>
       </div>
 
       <div className="hero max-h-screen bg-black">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img id="responsive" src="src/images/arakkep.jpg"className="max-w-lg rounded-lg shadow-2xl border-8 border-red-600"/>
+          <img
+            id="responsive"
+            src="src/images/arakkep.jpg"
+            className="max-w-lg rounded-lg shadow-2xl border-8 border-red-600"
+          />
           <div>
-            <h1 className="text-5xl font-bold text-white">Árak</h1>
+            <h1 className="text-5xl font-bold text-white">Áraink</h1>
             <p className="py-6 text-5xl font-bold text-white text-justify">
-            Az Ön igényeinakek megfelelő heti, havi bérletek, egy és több alkalmas órajegyek akár ajándékba is.
+              Az Ön igényeinakek megfelelő heti, havi bérletek, egy és több
+              alkalmas órajegyek akár ajándékba is.
             </p>
-            <button className="btn btn-primary hover:bg-red-600" id="price" value='/price'onClick={(e)=> handleButtonClick(e)}>Tovább az árakhoz</button>
+            <button
+              className="btn btn-primary hover:bg-red-600"
+              id="price"
+              value="/price"
+              onClick={(e) => handleButtonClick(e)}
+            >
+              Tovább az árakhoz
+            </button>
           </div>
         </div>
       </div>
-    
-    
     </div>
-
-      
   );
 }
 
--- components/Register.jsx
+++ components/Register.jsx
@@ -1,35 +1,40 @@
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 /*import {useNavigate} from 'react-router-dom';
 import UserContext from './context/UserContext';
 import {toast} from 'react-toastify';*/
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-  const navigate=useNavigate();
-    /*const{update}=useContext(UserContext);*/
+  const navigate = useNavigate();
+  /*const{update}=useContext(UserContext);*/
 
-    let formObj={
-        name:"",
-        phone:"",
-        email:"",
-        password:""
-    }
+  let formObj = {
+    name: "",
+    phone: "",
+    email: "",
+    password: "",
+  };
+  
+  
+  const again = () => {
+    window.Reload();
+  };
+  
+  const [formData, setFormData] = useState(formObj);
 
-    const [formData,setFormData]=useState(formObj);
+  const writeData = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
 
-    const writeData=(e)=>{
-        setFormData((prev)=>({...prev,[e.target.id]:e.target.value}))
-    }
-
-    const kuldes=(adat)=>{
-        fetch('http://localhost:80/api/fitness/register',{
-            method:'POST',
-            headers:{'Content-type':'application/json'},
-            body:JSON.stringify(adat)
-        })
-        .then(res=>res.json())
-        /*.then(token=>{
+  const kuldes = (adat) => {
+    fetch("http://localhost:80/api/fitness/register", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(adat),
+    })
+      .then((res) => res.json())
+      /*.then(token=>{
             if(!token.message){
                 sessionStorage.setItem('usertoken',token);
                 //toast.success('Sikeres regisztráció!',{position: toast.POSITION.BOTTOM_RIGHT})
@@ -42,95 +47,116 @@ function Register() {
                 alert(token.message);
             }
         })*/
-        //.catch(err=>toast.error(err,{position: toast.POSITION.BOTTOM_RIGHT}))
-        .then((result) => setSuccess(result))
-        .catch(error=>alert(error));
-    }
+      //.catch(err=>toast.error(err,{position: toast.POSITION.BOTTOM_RIGHT}))
+      .then((result) => {
+        setSuccess(result);
+        if (result) {
+          navigate('/login');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
-const [success,setSuccess]=useState();
+  const [success, setSuccess] = useState(true);
 
-const onSubmit=(e)=>{
-  e.preventDefault();
-  kuldes(formData);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    kuldes(formData);
+  };
+
   
-  
-}
 
   return (
-   <div>
-    <div className="hero max-h-screen bg-red-600">
-      <div className="hero-content flex-col lg:flex-row">
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <form>
-              <label className="label">
-                <span className="label-text font-bold text-2xl">Név</span>
-              </label>
-                <input
-                  type="text"
-                  id="name"
-                  placeholder="Teljes név"
-                  className="input input-bordered border-x-black w-full max-w-xs"
-                  onChange={(e)=>writeData(e)}
-                />
-                 <label className="label">
-                <span className="label-text font-bold text-2xl">Telefon</span>
-              </label>
-                <input
-                  type="phone"
-                  id="phone"
-                  placeholder="Telefonszám +36 ..........."
-                  className="input input-bordered border-x-black w-full max-w-xs"
-                  onChange={(e)=>writeData(e)}
-                />
-                <label className="label">
-                <span className="label-text font-bold text-2xl">Email</span>
-              </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Email"
-                  className="input input-bordered border-x-black w-full max-w-xs"
-                  onChange={(e)=>writeData(e)}
-                />
-                <label className="label">
-                <span className="label-text font-bold text-2xl">Jelszó</span>
-              </label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Jelszó"
-                  className="input input-bordered border-x-black w-full max-w-xs"
-                  onChange={(e)=>writeData(e)}
-                />
-               
-              </form>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-black" onClick={(e)=> onSubmit(e)}>Elküld</button>
-              {!success ? <p className="text-red-500 mt-4">Sikertelen regisztráció!</p>:<></>
-              }
+    <div>
+      <div className="hero max-h-screen bg-red-600">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <form>
+                  <label className="label">
+                    <span className="label-text font-bold text-2xl">Név</span>
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="Teljes név"
+                    className="input input-bordered border-x-black w-full max-w-xs"
+                    onChange={(e) => writeData(e)}
+                  />
+                  <label className="label">
+                    <span className="label-text font-bold text-2xl">
+                      Telefon
+                    </span>
+                  </label>
+                  <input
+                    type="phone"
+                    id="phone"
+                    placeholder="Telefonszám +36 ..........."
+                    className="input input-bordered border-x-black w-full max-w-xs"
+                    onChange={(e) => writeData(e)}
+                  />
+                  <label className="label">
+                    <span className="label-text font-bold text-2xl">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    className="input input-bordered border-x-black w-full max-w-xs"
+                    onChange={(e) => writeData(e)}
+                  />
+                  <label className="label">
+                    <span className="label-text font-bold text-2xl">
+                      Jelszó
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Jelszó"
+                    className="input input-bordered border-x-black w-full max-w-xs"
+                    onChange={(e) => writeData(e)}
+                  />
+                </form>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-black" onClick={(e) => onSubmit(e)}>
+                  Elküld
+                </button>
+                {!success ? (
+                  <p className="text-red-500 mt-4">Sikertelen regisztráció!</p>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <Link onClick={again} to="/register" id="link">
+          <button
+            className="bg-blue-600 hover:bg-red-500 text-white font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline items-center"
+            type="button"
+          >
+            Újra
+          </button>
+          </Link>
             </div>
           </div>
-        </div>
-        <div>
-          <h1 className="text-5xl font-bold">REGISZTRÁCIÓ</h1>
-          <p>
-            <label className="label">
-              <a
-                href="Login"
-                className="label-text-alt font-bold text-xl link link-hover"
-              >
-                Már regisztráltál? Itt bejelentkezhetsz.
-              </a>
-            </label>
-          </p>
+          <div>
+            <h1 className="text-5xl font-bold">REGISZTRÁCIÓ</h1>
+            <p>
+              <label className="label">
+                <a
+                  href="Login"
+                  className="label-text-alt font-bold text-xl link link-hover"
+                >
+                  Már regisztráltál? Itt bejelentkezhetsz.
+                </a>
+              </label>
+            </p>
+          </div>
         </div>
       </div>
-      
-    </div>
-    <div className="bg-black">
+      <div className="bg-black">
         <Link to="/">
           <button
             className="bg-blue-600 hover:bg-red-500 text-white font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline items-center"
@@ -140,7 +166,7 @@ const onSubmit=(e)=>{
           </button>
         </Link>
       </div>
-    </div> 
+    </div>
   );
 }
 
